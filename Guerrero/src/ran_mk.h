4d24dc41a33636fa384a50c978e151496264c08c
Zeeshawn Hasnain <zahasnai@ncsu.edu>
2024-02-15 11:04:09 -0500

    Removed boost from ran_mk.h

--- Guerrero/src/ran_mk.h
+++ Guerrero/src/ran_mk.h
@@ -17,25 +17,40 @@
 
 
 #include <random>
+using rng_uniform_int_t = std::uniform_int_distribution<>;
+using rng_uniform_real_t = std::uniform_real_distribution<>;
+using rng_exponential_t = std::exponential_distribution<>;
+using rng_binomial_t = std::binomial_distribution<>;
 
-#include <boost/random.hpp>
-using boost::variate_generator;
+// BOOST PARTS
+//#include <boost/random.hpp>
+//using boost::variate_generator;
 
 extern std::mt19937_64 gen;	// Declared externally in main.cpp so that it can be initialized there and called in random.cpp
 
-typedef variate_generator<std::mt19937&, std::uniform_int_distribution<> > rng_uniform_int_t;
-typedef variate_generator<std::mt19937&, std::uniform_real_distribution<> > rng_uniform_real_t;
-typedef variate_generator<std::mt19937&, std::exponential_distribution<> > rng_exponential_t;
-typedef variate_generator<std::mt19937&, std::binomial_distribution<> > rng_binomial_t;
+// BOOST PARTS
+//typedef variate_generator<std::mt19937&, std::uniform_int_distribution<> > rng_uniform_int_t;
+//typedef variate_generator<std::mt19937&, std::uniform_real_distribution<> > rng_uniform_real_t;
+//typedef variate_generator<std::mt19937&, std::exponential_distribution<> > rng_exponential_t;
+//typedef variate_generator<std::mt19937&, std::binomial_distribution<> > rng_binomial_t;
 
 
+// BOOST PARTS
+//template<class PRNG, class Dist>
+//inline variate_generator<PRNG&, Dist> make_gen(PRNG & rng, Dist d)
+//{
+//  return variate_generator<PRNG&, Dist>(rng, d);
+//}
 
+/**
+ * Functions the same as above method without using boost libraries
+ * @author Zeeshawn Hasnain
+ */
 template<class PRNG, class Dist>
-inline variate_generator<PRNG&, Dist> make_gen(PRNG & rng, Dist d)
+inline typename Dist::result_type make_gen(PRNG& rng, Dist d)
 {
-  return variate_generator<PRNG&, Dist>(rng, d);
+    return d(rng);
 }
-
 
 
 inline rng_uniform_int_t::result_type randint(int lower_bound, int upper_bound)
@@ -43,7 +58,7 @@ inline rng_uniform_int_t::result_type randint(int lower_bound, int upper_bound)
 //
 // randint(a, b) returns a uniformly distributed random integer on [a, b]
 //
-    return make_gen(gen, std::uniform_int_distribution<>(lower_bound, upper_bound))();
+    return make_gen(gen, std::uniform_int_distribution<>(lower_bound, upper_bound));
 }
 
 
@@ -55,7 +70,7 @@ inline rng_uniform_real_t::result_type randreal(double lower_bound = 0, double u
 //	randreal() is uniform on [0, 1]
 //	randreal(a, b) is uniform on [a, b].
 
-    return make_gen(gen, std::uniform_real_distribution<>(lower_bound, upper_bound))();
+    return make_gen(gen, std::uniform_real_distribution<>(lower_bound, upper_bound));
 }
 
 
@@ -65,11 +80,11 @@ inline rng_exponential_t::result_type randexp(double lambda)
 //
 // Returns a random deviate from an exponential distribution with parameter lambda
 //
-    return make_gen(gen, std::exponential_distribution<>(lambda))();
+    return make_gen(gen, std::exponential_distribution<>(lambda));
 }
 
+using dist_t = std::poisson_distribution<int>;
 inline int randpois( double n ) {
-    using dist_t = std::poisson_distribution<int>;
     static dist_t d{n};
     return d(gen);
 }
@@ -79,7 +94,7 @@ inline rng_binomial_t::result_type randbinom(unsigned int trials, double p)
     //
     // Returns a random deviate from an poisson distribution with parameter lambda
     //
-    return make_gen(gen, std::binomial_distribution<>(trials, p))();
+    return make_gen(gen, std::binomial_distribution<>(trials, p));
 }
 
 #endif //RANDOM_
